785ad8c7757f54c6aa13360beb01142a1749ab59
MrNgoc96 <mrboyvip.sl96@gmail.com>
2017-11-03 09:33:10 +0700

    comit 2
    dsadsadsa

--- src/main/java/hangrong/controller/HomeController.java
+++ src/main/java/hangrong/controller/HomeController.java
@@ -1,20 +1,10 @@
 package hangrong.controller;
 
-
-
-
-import java.io.File;
-
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.AbstractController;
+
 
 
 
@@ -22,7 +12,13 @@ import org.springframework.web.servlet.mvc.AbstractController;
 public class HomeController {
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	protected String home(Model model) {
+	protected String home(HttpServletRequest request) {
+		request.getSession().setAttribute("currentUser", null);
+		return "index";
+	}
+	
+	@RequestMapping(value = "/home", method = RequestMethod.GET)
+	protected String home() {
 		return "index";
 	}
    
